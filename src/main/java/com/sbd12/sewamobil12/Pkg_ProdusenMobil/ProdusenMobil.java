852f7c2880d406fb1ef2618943f69527eab21353
ArieDZ_2 <ArieDZ_2@DESKTOP-HNOOKEG>
2017-10-24 00:45:27 +0700

    BASE

---
+++ src/main/java/com/sbd12/sewamobil12/Pkg_ProdusenMobil/ProdusenMobil.java
@@ -0,0 +1,29 @@
+package com.sbd12.sewamobil12.Pkg_ProdusenMobil;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ArieDZ_2
+ */
+public class ProdusenMobil {
+   private String id_produsen;
+   private String nama_produsen;
+
+   public void setId_produsen(String id) {
+      this.id_produsen = id;
+   }
+   public String getId_produsen() {
+      return id_produsen;
+   }
+   public void setNama_produsen(String nama) {
+      this.nama_produsen = nama;
+   }
+   public String getNama_produsen() {
+      return nama_produsen;
+   }
+}
---
+++ src/main/java/com/sbd12/sewamobil12/Pkg_ProdusenMobil/ProdusenMobilMapper.java
@@ -0,0 +1,33 @@
+package com.sbd12.sewamobil12.Pkg_ProdusenMobil;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ArieDZ_2
+ */
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
+public class ProdusenMobilMapper implements RowMapper<ProdusenMobil> {
+
+    /**
+     *
+     * @param rs
+     * @param rowNum
+     * @return
+     * @throws SQLException
+     */
+    @Override
+   public ProdusenMobil mapRow(ResultSet rs, int rowNum) throws SQLException {
+      ProdusenMobil produsen = new ProdusenMobil();
+      produsen.setId_produsen(rs.getString("id_produsen_mobil"));
+      produsen.setNama_produsen(rs.getString("nama_produsen"));
+      return produsen;
+   }
+}
